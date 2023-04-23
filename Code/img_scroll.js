213787a69a187563bc0b6ad0ebf85b2548afbb28
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2023-04-23 16:21:03 +0300

    ONGOING, unchecked

--- Code/img_scroll.js
+++ Code/img_scroll.js
@@ -1,35 +1,38 @@
 // img_scroll.js
 ////////////////////////////////////////////////////////////////////////////////
 
-// TODO: pass 'tempo', 'tactsInLine', 'beatsInTact' from the HTML
-// TODO: provide a way for user to force alternative 'tempo'
-var tempo = 60; // beats-per-minute
-var tactsInLine = 7;
-var beatsInTact = 4;
+// TODO: pass 'g_tempo', 'g_tactsInLine', 'g_beatsInTact' from the HTML
+// TODO: provide a way for user to force alternative 'g_tempo'
+var g_tempo = 60; // beats-per-minute
+var g_tactsInLine = 7;
+var g_beatsInTact = 4;
 
 
-const beatsForFullLine = tactsInLine * beatsInTact;
-const fullLineInSec = beatsForFullLine * tempo / 60;
+const g_beatsForFullLine = g_tactsInLine * g_beatsInTact;
+const g_fullLineInSec = g_beatsForFullLine * g_tempo / 60;
 
-var currStep = -1;  // not started
-var scrollIsOn = false;
+var g_currStep = -1;  // not started
+var g_scrollIsOn = false;
+
+var g_nextTimerIntervalId = 0;
+
 
 // TODO: specify target coordinates for image, not HTML page
 // TODO, pass the whole array from the HTML
-var scoreStations = [
-  {tag:"line-001-Begin", pageId:"pg01", x:0, y:656,  pause:fullLineInSec},
-  {tag:"line-002-Begin", pageId:"pg01", x:0, y:1044, pause:fullLineInSec},
-  {tag:"line-003-Begin", pageId:"pg01", x:0, y:1464, pause:2/3*fullLineInSec},
-  {tag:"line-001-Begin", pageId:"pg01", x:0, y:656,  pause:fullLineInSec},
-  {tag:"line-002-Begin", pageId:"pg01", x:0, y:1044, pause:fullLineInSec},
-  {tag:"line-003-Begin", pageId:"pg01", x:0, y:1464, pause:2/3^fullLineInSec},
-  {tag:"line-004-Begin", pageId:"pg01", x:0, y:1840, pause:fullLineInSec},
-  {tag:"line-005-Begin", pageId:"pg01", x:0, y:2220, pause:fullLineInSec},
-  {tag:"line-006-Begin", pageId:"pg01", x:0, y:2592, pause:2/3*fullLineInSec},
-  {tag:"line-003-Begin", pageId:"pg01", x:0, y:1464, pause:fullLineInSec},
-  {tag:"line-004-Begin", pageId:"pg01", x:0, y:1840, pause:fullLineInSec},
-  {tag:"line-005-Begin", pageId:"pg01", x:0, y:2220, pause:fullLineInSec},
-  {tag:"line-006-Begin", pageId:"pg01", x:0, y:2592, pause:2/3*fullLineInSec},  
+var g_scoreStations = [timeSec
+  {tag:"line-001-Begin", pageId:"pg01", x:0, y:656,  timeSec:g_fullLineInSec},
+  {tag:"line-002-Begin", pageId:"pg01", x:0, y:1044, timeSec:g_fullLineInSec},
+  {tag:"line-003-Begin", pageId:"pg01", x:0, y:1464, timeSec:2/3*g_fullLineInSec},
+  {tag:"line-001-Begin", pageId:"pg01", x:0, y:656,  timeSec:g_fullLineInSec},
+  {tag:"line-002-Begin", pageId:"pg01", x:0, y:1044, timeSec:g_fullLineInSec},
+  {tag:"line-003-Begin", pageId:"pg01", x:0, y:1464, timeSec:2/3^g_fullLineInSec},
+  {tag:"line-004-Begin", pageId:"pg01", x:0, y:1840, timeSec:g_fullLineInSec},
+  {tag:"line-005-Begin", pageId:"pg01", x:0, y:2220, timeSec:g_fullLineInSec},
+  {tag:"line-006-Begin", pageId:"pg01", x:0, y:2592, timeSec:2/3*g_fullLineInSec},
+  {tag:"line-003-Begin", pageId:"pg01", x:0, y:1464, timeSec:g_fullLineInSec},
+  {tag:"line-004-Begin", pageId:"pg01", x:0, y:1840, timeSec:g_fullLineInSec},
+  {tag:"line-005-Begin", pageId:"pg01", x:0, y:2220, timeSec:g_fullLineInSec},
+  {tag:"line-006-Begin", pageId:"pg01", x:0, y:2592, timeSec:2/3*g_fullLineInSec},  
 ];
   
 // To facilitate passing parameters to event handlers, use an anonymous function
@@ -46,13 +49,13 @@ function scroll__onload(x, y)
   //~ alert(`Page onload event; will scroll to ${x}, ${y}`);
   //~ window.scrollTo(x, y);
   
-  const firstPageId = scoreStations[0].pageId;
+  const firstPageId = g_scoreStations[0].pageId;
   const firstPage = document.getElementById(firstPageId);
   const topPos = firstPage.offsetTop;
   alert(`Page onload event; scroll to the first page (${firstPageId}) at y=${topPos}`);
   window.scrollTo({ top: topPos, behavior: 'smooth'});
-  scrollIsOn = false;
-  currStep = -1;
+  g_scrollIsOn = false;
+  g_currStep = -1;
 }
 
 function message__onMouseOver(event)
@@ -64,24 +67,48 @@ function message__onMouseOver(event)
 function scroll_start_handler(event)
 {
   event.preventDefault();
-  if (currStep == -1) {
-    currStep = 0;
+  if ( g_currStep == -1 ) {
+    g_currStep = 0;
     alert(`START SCROLLING FROM THE TOP`);
-  } else if (scrollIsOn == true)  {
-    currStep = 0;
+  } else if ( g_scrollIsOn == true )  {
+    g_currStep = 0;
     alert(`START SCROLLING FROM THE TOP`);
-  } else  {  // scrollIsOn == falsa
-    alert(`RESUME SCROLLING FROM STEP ${currStep}`);
+  } else  {  // g_scrollIsOn == falsa
+    alert(`RESUME SCROLLING FROM STEP ${g_currStep}`);
   }
-  scrollIsOn = true;
+  g_scrollIsOn = true;
+  scroll_schedule(g_scoreStations[g_currStep].timeSec)
 }
 
 
 function scroll_stop_handler(event)
 {
   alert(`onLeftClick event on ${event.target.id};\tSTOP SCROLLING`);
-  scrollIsOn = false;
+  scroll_abort();
 }
+
+
+function scroll_schedule(currDelaySec)
+{
+  g_nextTimerIntervalId = setTimeout(scroll_step_handler, currDelaySec);
+}
+
+
+function scroll_abort()
+{
+  g_scrollIsOn = false;
+  clearInterval(g_nextTimerIntervalId);
+  g_nextTimerIntervalId = 0;
+}
+
+
+// Determines the current step number and executes the step
+function scroll_step_handler()
+{
+  stepNum = g_currStep; // TODO: track actual window scroll position
+  return  scroll_one_step(stepNum);
+}
+
 
 function messge_onKeyPress(event)
 {
@@ -90,4 +117,25 @@ function messge_onKeyPress(event)
 
 
 //////////////////////////////////////////////////
-function scrol_one_step(
+/* Scrolls to the current-step position and starts timer to requested interval
+ */
+function scroll_one_step(stepNum)
+{
+  if ( g_scrollIsOn == false )  { return }
+  if ( (stepNum < 0) || (stepNum > g_scoreStations.length) )  {
+    console.log(`-I- At step number ${stepNum}; stop scrolling`);
+    scroll_abort();
+    return  0;
+  }
+  //  {tag:"line-001-Begin", pageId:"pg01", x:0, y:656,  timeSec:g_fullLineInSec}
+  const rec = g_scoreStations[stepNum];
+  const currPage = document.getElementById(rec.pageId);
+  const currPos = currPage.offsetTop + rec.y;
+
+  console.log('-I- Scroll to ${rec.pageId}:${currPos}) for step ${stepNum}');
+  window.scrollTo({ top: currPos, behavior: 'smooth'});
+  g_currStep = stepNum + 1;
+  // the next line causes async wait
+  scroll_schedule(rec.timeSec);
+  return  1;
+}
