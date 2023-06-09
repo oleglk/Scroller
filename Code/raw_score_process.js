04bf0b13860e823421c131cc025b642275b7648c
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2023-06-09 17:34:37 +0300

    ONGOING: scan_one_page()

--- Code/raw_score_process.js
+++ Code/raw_score_process.js
@@ -1,5 +1,6 @@
 // raw_score_process.js
 
+var pageIdsAndImagePaths = null;  // OK_TMP
 
 /* To facilitate passing parameters to event handlers, use an anonymous function
  * Wrap it by named wrapper to allow storing the handler for future removal */
@@ -18,8 +19,18 @@ function raw__onload(event)
   document.title = pageName;
 
   // find all score images on the page
-  let imagePathsUnsorted = Array.prototype.map.call(
-    document.images, function (img) {
-      console.log('image', img.src); });
+  // let pageIdsAndImagePaths = Array.prototype.map.call(
+  //   document.images, function (img) {
+  //     console.log('image', img.src); });
+  /*let*/ pageIdsAndImagePaths = Array.prototype.map.call(
+    document.images,    (x)  =>  { return {id: x.id,  url: x.src}; }  );
+  // build 'pageImagePathsMap' ==  {pageId:STR => imgPath:STR*}
+  let pageImagePathsMap = new Map();
+  for ( let iAndP of pageIdsAndImagePaths.entries() ) {
+    console.log(`-I- Image [${iAndP[0]}] : "${iAndP[1].id}"="${iAndP[1].url}"`);
+    pageImagePathsMap.set( iAndP[1].id, iAndP[1].url );
+  }
+
+  // TODO: analyze
 }
 // wrapper had to be moved to the top - before the 1st use
