260f667d649c02fd376090b5492dd49b82b0e48c
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2023-07-01 12:41:48 +0300

    ONGOING: scroll to prev line

--- Code/PlayOrder.js
+++ Code/PlayOrder.js
@@ -195,6 +195,8 @@ _DBG__scoreDataLines = this.scoreDataLines;  // OK_TMP: reveal for console
     ////for ( const [i, playedLine] of this.linePlayOrder.entries() )  {}
     // TODO: any precautions to ensure last line(s) are visible in the last step?
     for ( let i = 0;  i < this.linePlayOrder.length;  i += numLinesInStep )  {
+      let isFirst = (i == 0);
+      //let isLast  = (i == (this.linePlayOrder.length - 1));
       let playedLine = this.linePlayOrder[i];
       let scoreLine  = this.scoreDataLines.find(
         (element, index, array) => ((element.pageId  == playedLine.pageId) &&
@@ -204,14 +206,28 @@ _DBG__scoreDataLines = this.scoreDataLines;  // OK_TMP: reveal for console
         console.log(err);  console.trace();  alert(err);
         return  null;
       }
+      // prefer scroll to the line before the one played to give spare time
+      let prevPlayedLine = (isFirst)? null : this.linePlayOrder[i-1];
+      let prevScoreLine  = (isFirst)? null : this.scoreDataLines.find(
+        (element,index,array) => ((element.pageId  == prevPlayedLine.pageId) &&
+                                  (element.lineIdx == prevPlayedLine.lineIdx)));
+      if ( !isFirst && (prevPlayedLine == undefined) )  {
+        err = `Missing score data line page:${prevPlayedLine.pageId}, local-index=${prevPlayedLine.lineIdx}`;
+        console.log(err);  console.trace();  alert(err);
+        return  null;
+      }
       let stepTag = "step:" + zero_pad(scoreStationsArray.length, 2);
-      
+      const scoreLineToScrollTo = (!isFirst)? prevScoreLine : scoreLine;
+      // TODO: from which line to take 'pageId'+'lineIdx' ???
+      // !!! TODO: special treatment to 'bottom' line if played several times!!!
+      // !!! (Example: the last line in Papirossen) !!!
+
       let station = { tag:           stepTag,
                       pageId:        this.playedLinePageOccurences[i],
                       origImgPageId: playedLine.pageId,
                       lineOnPageIdx: playedLine.lineIdx,
                       x:             0,
-                      y:             scoreLine.yOnPage,
+                      y:             scoreLineToScrollTo.yOnPage,
                       timeSec:       playedLine.timeBeat * 60.0 / this.tempo};
       scoreStationsArray.push( station );
     }
--- Code/img_scroll.js
+++ Code/img_scroll.js
@@ -25,7 +25,7 @@ window.addEventListener( 'unhandledrejection', function (e) {
 
 
 //////// Begin: scroller application configuration stuff ///////////////////////
-const g_numLinesInStep = 2; // HARDCODED(!) number of lines to scroll in one step
+const g_numLinesInStep = 1; // 1|2 HARDCODED(!) number of lines to scroll in one step
 const g_progressShowPeriodSec = 1;
 const g_progressBar_numCellsForMinFullTime = 3;
 const g_progressBar_fontSize = 25;
